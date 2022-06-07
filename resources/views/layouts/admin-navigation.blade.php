d0a04367675e2d014e60c4181bbdd3f9ac1e2269
Yudai35 <yuusongben14@gmail.com>
2022-06-07 22:15:32 +0900

    Carbon

--- resources/views/layouts/admin-navigation.blade.php
+++ resources/views/layouts/admin-navigation.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <div class="w-12">
+                    <div class="w-8">
                         <a href="{{ route('admin.dashboard') }}">
                             <x-application-logo class="block h-10  fill-current text-gray-600" />
                         </a>
