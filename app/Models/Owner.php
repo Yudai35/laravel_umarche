6797f0b114257e8fc935992bebbf96bd98035d76
Yudai35 <yuusongben14@gmail.com>
2022-06-18 09:59:06 +0900

    imega index

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\softDeletes;
 use App\Models\Shop;
+use App\Models\Image;
+
 
 
 class Owner extends Authenticatable
@@ -46,5 +48,10 @@ class Owner extends Authenticatable
     public function shop()
     {
         return $this->hasOne(Shop::class);
+    }
+
+    public function image()
+    {
+        return $this->hasMany(Image::class);
     }
 }
