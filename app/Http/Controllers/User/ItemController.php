7c43ff078ac136b2d67797daeb2638e5b260a78f
Yudai35 <yuusongben14@gmail.com>
2022-06-27 22:41:48 +0900

    商品情報の在庫情報

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Stock;
 
 class ItemController extends Controller
 {
@@ -56,7 +57,9 @@ class ItemController extends Controller
     public function show($id)
     {
         $product = Product::findOrFail($id);
+        $quantity = Stock::where('product_id', $product->id)
+            ->sum('quantity');
 
-        return view('user.show', compact('product'));
+        return view('user.show', compact('product', 'quantity'));
     }
 }
