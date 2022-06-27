ac698e1efb0222afedd8bfdb8fef405bacff9d83
Yudai35 <yuusongben14@gmail.com>
2022-06-27 22:12:48 +0900

    ショップ情報

--- database/seeders/ShopSeeder.php
+++ database/seeders/ShopSeeder.php
@@ -21,14 +21,14 @@ class ShopSeeder extends Seeder
                 'owner_id' => 1,
                 'name' => 'ここに店名が入る',
                 'information' => 'ここにお店の情報が入ります。ここにお店の情報が入ります。ここにお店の情報が入ります',
-                'filename' => '',
+                'filename' => 'sample1.jpeg',
                 'is_selling' => true
             ],
             [
                 'owner_id' => 2,
                 'name' => 'ここに店名が入る',
                 'information' => 'ここにお店の情報が入ります。ここにお店の情報が入ります。ここにお店の情報が入ります',
-                'filename' => '',
+                'filename' => 'sample2.jpeg',
                 'is_selling' => true
             ],
 
